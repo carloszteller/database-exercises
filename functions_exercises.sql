deba679bd2258073b63874a3e107a11d9bdc8460
Carlos Teller <carlos.z.teller@gmail.com>
2020-04-16 10:57:05 -0500

    Corrected to display those employees who last name starts and ends with an 'E'

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,6 +1,6 @@
 \W
 use employees;
 
-select concat(first_name, ' ', last_name) from employees where last_name like 'E%' order by emp_no;
+select concat(first_name, ' ', last_name) from employees where last_name like 'E%' and last_name like '%E' order by emp_no;
 
 select concat(first_name, ' ', last_name, ': ', datediff(now(), hire_date), ' days') from employees where hire_date like '199%' and birth_date like '%12-25' order by birth_date, hire_date desc;
